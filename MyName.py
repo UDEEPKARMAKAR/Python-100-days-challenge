365a7b70a17037d9cf550cb82ef9f97c6002b166
UDEEPKARMAKAR <udeepkarmakar2006@gmail.com>
2025-04-01 00:09:54 +0530

    day 1

---
+++ MyName.py
@@ -0,0 +1,1 @@
+print("Udeep Karmakar")
---
+++ calculator.py
@@ -0,0 +1,6 @@
+a=5
+b=8
+print("The value of", a, "+", b, "is", a+b)
+print("The value of", a, "-", b, "is", a-b)
+print("The value of", a, "*", b, "is", a*b)
+print("The value of", a, "/", b, "is", a/b)
