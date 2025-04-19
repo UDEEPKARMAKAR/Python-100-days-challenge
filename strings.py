25fd3872281cf417205c11a0bcffe316d9c74ae8
UDEEPKARMAKAR <udeepkarmakar2006@gmail.com>
2025-04-19 20:45:41 +0530

    Day 4

---
+++ strings.py
@@ -0,0 +1,22 @@
+name = "Udeep"
+friend = "Swastik"
+anotherFriend = "Yunus"
+print ("Hello! " + name)
+print (name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+#print(name[5]) throws an error
+#we can also use "for lop"
+print ("lets use a for loop")
+for character in name:
+    print(character)
+
+
+st = """Hello!
+What are you doing?
+how do you do?"""
+print (st)
+for character in st:
+    print(character)
